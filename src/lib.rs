cd514efbe2c8d211d994bffb229b375094b55f35
KENTARO OKUDA <lonesometraveler@mac.com>
2020-03-16 19:19:02 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,34 +7,6 @@
 //! ### Datasheets
 //! - [MAX6955](https://datasheets.maximintegrated.com/en/ds/MAX6955.pdf)
 //!
-//! # Examples
-//!```rust
-//!#[entry]
-//!fn main() -> ! {
-//!    let dp = stm32f30x::Peripherals::take().unwrap();
-//!    let mut flash = dp.FLASH.constrain();
-//!    let mut rcc = dp.RCC.constrain();
-//!
-//!    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-//!
-//!    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
-//!    let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
-//!    let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
-//!
-//!    let i2c = I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
-//!
-//!    // create an instance with the default address 0x60
-//!    let mut max6955 = Max6955::new(i2c).unwrap();
-//!    // power up
-//!    max6955.powerup().unwrap();
-//!    // set intensity
-//!    max6955.set_global_intensity(4).unwrap();
-//!    // write text
-//!    max6955.write_str("HELLO").unwrap();
-//!
-//!    loop {}
-//!}
-//!```
 
 // #![deny(missing_docs)]
 #![deny(warnings)]
